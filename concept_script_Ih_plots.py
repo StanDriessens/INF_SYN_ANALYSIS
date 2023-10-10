cb3f07fef908f4a8e31b55cc51b11538d36600dd
StanDriessens <standriessens@gmail.com>
2023-10-10 09:47:30 +0200

    Initial commit

---
+++ concept_script_Ih_plots.py
@@ -0,0 +1,231 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 12:57:01 2023
+
+analye Ih experimetn events 
+
+@author: sdr267
+"""
+
+import os 
+from pathlib import Path
+import seaborn as sns
+import pandas as pd
+
+#%%load in the data 
+path_recov = r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\human\control'
+files = Path(path_recov).glob('*.csv')
+# load in the files
+dfs = list()
+for f in files:
+    data = pd.read_csv(f)
+    data['file']= f.stem
+    dfs.append(data)
+df_human_control = pd.concat(dfs, ignore_index=True)
+df_human_control['condition'] = 'control'
+
+path_recov = r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\human\zd'
+files = Path(path_recov).glob('*.csv')
+# load in the files
+dfs = list()
+for f in files:
+    data = pd.read_csv(f)
+    data['file']= f.stem
+    dfs.append(data)
+df_human_zd = pd.concat(dfs, ignore_index=True)
+df_human_zd['condition'] = 'zd'
+
+path_recov = r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\mouse\control'
+files = Path(path_recov).glob('*.csv')
+# load in the files
+dfs = list()
+for f in files:
+    data = pd.read_csv(f)
+    data['file']= f.stem
+    dfs.append(data)
+df_mouse_control = pd.concat(dfs, ignore_index=True)
+df_mouse_control['condition'] = 'control'
+
+path_recov = r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\mouse\zd'
+files = Path(path_recov).glob('*.csv')
+# load in the files
+dfs = list()
+for f in files:
+    data = pd.read_csv(f)
+    data['file']= f.stem
+    dfs.append(data)
+df_mouse_zd = pd.concat(dfs, ignore_index=True)
+df_mouse_zd['condition'] = 'zd'
+#%%concentante to tota 
+df_total = pd.concat([df_human_control, df_human_zd, df_mouse_control, df_mouse_zd], axis=0 , ignore_index=True)
+df_total['file'] = df_total['file_name'].apply(lambda x: os.path.basename(x))
+#only select qualtiy pass
+
+df_total_pass = df_total.loc[df_total['qc'] == 'p']
+
+#load in the metadata 
+
+metadata=pd.read_csv(r'C:/Users/sdr267/Documents/PhD/ProjectSynapticConnections/Ih_experiment/metadata.csv')
+#triangulate distance using pythagoras 
+metadata['distance'] = (metadata['x_dif']**2 + metadata['y_dif']**2)**0.5
+
+#merge metadta with the ephys data
+
+df_total_pass = pd.merge(df_total_pass, metadata, on='file') 
+
+#calculate uM/ms 
+
+df_total_pass['speed']= df_total_pass['distance']/df_total_pass['0']
+
+#get normalized speeds aswell
+
+#%%
+#plot some data distributions
+import seaborn as sns
+import matplotlib.pyplot as plt
+mouse_data = df_total_pass.loc[df_total_pass['species_y'] == 'mouse'].groupby(['file_name', 'condition']).mean().reset_index()
+human_data = df_total_pass.loc[df_total_pass['species_y'] == 'human'].groupby(['file_name', 'condition']).mean().reset_index()
+
+
+
+colors_scheme = {'#C875C4', '#32cd32'}
+
+
+plt.figure(figsize=(18,18))
+sns.lineplot(x='condition', y='speed', data=mouse_data, palette=(colors_scheme))
+sns.boxplot(x='condition', y='speed', data=mouse_data, medianprops=dict(color="black", alpha=1, linewidth=4), boxprops=dict(linewidth=1.5, facecolor='w', edgecolor='w', alpha=1), 
+                 whiskerprops=dict(linewidth=4, color='k', alpha=1), capprops=dict(linewidth=4, color='k', alpha=1))
+plt.ylabel('synaptic delay (ms)', fontsize=30)
+plt.yticks(fontsize=30)
+plt.xticks(fontsize=30)
+plt.xlabel('condition', fontsize=30)
+plt.tick_params(length=8)
+plt.tick_params(width=4)
+
+
+
+
+sns.boxplot(mouse_data, x='condition', y='0')
+sns.boxplot(human_data, x='condition', y='0')
+
+sns.pointplot(data=mouse_data, x='n_pulse', y='0', hue='condition', errorbar=('ci', 95), capsize=1)
+
+
+plt.figure(figsize=(15,25))
+
+unique_files = mouse_data['file_name'].unique()
+
+for file in unique_files:
+    file_data = mouse_data[mouse_data['file_name'] == file]
+    sns.lineplot(data=file_data, x='condition', y='normalized_speed', marker='o', linewidth =5,
+                 dashes=True, markers=True, linestyle='--', markersize=20)
+plt.ylabel('normalized signaling speed', fontsize=20)
+#plt.ylim(2.0, 6.0)
+plt.yticks(fontsize=20)
+plt.xticks(fontsize=20)
+plt.xlabel('condition', fontsize=20)
+plt.tick_params(length=8)
+plt.tick_params(width=4)
+
+plt.figure(figsize=(15,25))
+    
+unique_files = human_data['file_name'].unique()
+
+for file in unique_files:
+    file_data = human_data[human_data['file_name'] == file]
+    sns.lineplot(data=file_data, x='condition', y='normalized_speed', marker='o', linewidth =5,
+                 dashes=True, markers=True, linestyle='--', markersize=20, legend='full')
+plt.ylabel('normalized signaling speed', fontsize=20)
+#plt.ylim(2.0, 6.0)
+plt.yticks(fontsize=20)
+plt.xticks(fontsize=20)
+plt.xlabel('condition', fontsize=20)
+plt.tick_params(length=8)
+plt.tick_params(width=4)
+
+
+#%%calculate normalized speeds
+avg_speeds_control = human_data[human_data['condition'] == 'control'].groupby('file_name')['speed'].mean()
+human_data = human_data.merge(avg_speeds_control.reset_index(), on='file_name', suffixes=('', '_avg_control'))
+
+human_data['normalized_speed'] = human_data['normalized_speed'] = human_data['speed'] / human_data['speed_avg_control']
+#mouse
+avg_speeds_control = mouse_data[mouse_data['condition'] == 'control'].groupby('file_name')['speed'].mean()
+mouse_data = mouse_data.merge(avg_speeds_control.reset_index(), on='file_name', suffixes=('', '_avg_control'))
+
+mouse_data['normalized_speed'] = mouse_data['normalized_speed'] = mouse_data['speed'] / mouse_data['speed_avg_control']
+
+human_zd = human_data.loc[human_data['condition']== 'zd'].reset_index()
+mouse_zd = mouse_data.loc[mouse_data['condition']== 'zd'].reset_index()
+
+human_zd['species']= 'human'
+mouse_zd['species']= 'mouse'
+
+total_zd = pd.concat([mouse_zd, human_zd], axis=0)
+
+
+sns.lmplot(data=human_data, x='distance', y = 'speed', hue='condition', ci = None)
+
+
+
+sns.lmplot(data=human_zd, x='distance', y = '0', ci = None, markers='x', palette='k')
+plt.ylabel('normalized signaling speed', fontsize=15)
+plt.xlabel('distance from stimulation electrode to soma (um)', fontsize =15)
+plt.yticks(fontsize=15)
+plt.xticks(fontsize=15)
+
+sns.lmplot(data=mouse_zd, x='distance', y = 'normalized_speed', ci = None)
+
+sns.lmplot(data=total_zd, x='distance', y = 'normalized_speed', hue='species', ci=None)
+
+
+#differences for pulse numbers\
+mouse_data_pulse = df_total_pass.loc[df_total_pass['species_y'] == 'mouse'].groupby(['file_name', 'condition', 'n_pulse']).mean().reset_index()
+human_data_pulse = df_total_pass.loc[df_total_pass['species_y'] == 'human'].groupby(['file_name', 'condition', 'n_pulse']).mean().reset_index()
+
+sns.lineplot(data=mouse_data_pulse, x='n_pulse', y = '0', hue='condition', errorbar=('ci', 95), style="condition", markers=True)
+plt.yticks(fontsize=15)
+plt.xticks(fontsize=15)
+
+plt.ylabel('signal latency (ms)', fontsize= 15)
+plt.xlabel('external stimulation pulse number', fontsize= 15)
+
+sns.lineplot(data=human_data_pulse, x='n_pulse', y = 'speed', hue='condition', errorbar=('ci', 0), style="condition", markers=True)
+plt.yticks(fontsize=15)
+plt.xticks(fontsize=15)
+
+unique_files = human_data_pulse['file_name'].unique()
+
+for file in unique_files:
+    file_data = human_data_pulse[human_data_pulse['file_name'] == file]
+    sns.lineplot(data=file_data, x='n_pulse', y='speed', marker='o', linewidth =1,
+                 dashes=True, markers=True, linestyle='--', markersize=1, legend='full', hue = 'condition')
+
+
+plt.ylabel('signal latency (ms)', fontsize= 15)
+plt.xlabel('external stimulation pulse number', fontsize= 15)
+
+#%%comapre mouse to human speed
+
+
+df_control_total = df_total_pass[df_total_pass['condition'] == 'control'].groupby(['file_name', 'species_x']).mean().reset_index()
+ 
+
+colors_scheme = {'#C875C4', '#32cd32'}
+
+
+plt.figure(figsize=(10,10))
+sns.stripplot(x='species_x', y='speed', data=df_control_total, size=12, hue='species_x', palette=colors_scheme)
+sns.boxplot(x='species_x', y='speed', data=df_control_total, medianprops=dict(color="black", alpha=1, linewidth=4), boxprops=dict(linewidth=1.5, facecolor='w', edgecolor='w', alpha=1), 
+                 whiskerprops=dict(linewidth=4, color='k', alpha=1), capprops=dict(linewidth=4, color='k', alpha=1))
+plt.ylabel('signaling speed (um/ms)', fontsize=30)
+plt.yticks(fontsize=30)
+plt.xticks(fontsize=30)
+plt.xlabel('species', fontsize=30)
+plt.tick_params(length=8)
+plt.tick_params(width=4)
+
+
+import pingouin as pg 
+
+pg.mwu(df_control_total[df_control_total['species_x']=='human'].speed, df_control_total[df_control_total['species_x']=='mouse'].speed)
---
+++ concept_script_anaylsis_Ih_experiment.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  7 08:57:04 2023
+
+@author: sdr267
+"""
+
+from pathlib import Path
+import seaborn as sns
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+from pynwb import NWBFile, TimeSeries, NWBHDF5IO
+from pynwb.epoch import TimeIntervals
+from pynwb.file import Subject
+from pynwb.behavior import SpatialSeries, Position
+from datetime import datetime
+from dateutil import tz
+from ipfx.dataset.create import create_ephys_data_set
+from ipfx.feature_extractor import SpikeFeatureExtractor
+import h5py
+import matplotlib.pyplot as plt 
+from scipy.signal import order_filter
+from scipy.signal import butter,filtfilt
+import scipy as sp
+import pandas as pd
+import seaborn as sns
+import os
+from scipy.signal import find_peaks, peak_prominences
+import h5py
+import tkinter 
+from tkinter import filedialog
+from tkinter import messagebox as mb
+from tkinter import Radiobutton
+
+#%% SINGLE EVENT DETECION
+print('select file from the list')
+file = filedialog.askopenfilename()
+condition = []
+while condition != 'ZD' and condition != 'control':
+    condition = input('which protocol are you analyzing?, ZD or control?')
+    if condition == 'ZD':
+        cond = 'TTL_test_ZD'
+    elif condition == 'control':
+        cond = 'TTL_test_GAP'
+    else:
+        print('wrong input, try again')
+species = input('please provide species (human/mouse)')
+
+print('loading dataset')
+dataset= create_ephys_data_set(file)
+all_sweeps = dataset.filtered_sweep_table()
+print('fetching TTL data')
+#where is TTL??
+f = h5py.File(file, 'r')
+stim = pd.DataFrame(list(f['stimulus']['presentation'].keys()), columns=['ttl'])
+#create list with only TTL file names
+data_TTL = stim[stim['ttl'].str.contains('TTL')]
+
+#select subset of sweeps 
+subset = list(all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number)
+#convert to string for search
+strings = [str(x) for x in subset]
+#only select TTLs for subselected sweep numbers 
+selected_TTLs = data_TTL[data_TTL['ttl'].str.contains('|'.join(strings))].reset_index(drop=True)
+#select sweeps to analyze
+sweeps_to_analyze = all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number.reset_index(drop=True)
+
+response_onset = pd.DataFrame()
+
+for i in range(0,len(sweeps_to_analyze)):
+    sweeps = []
+    ttl = selected_TTLs.ttl[i]
+    ttl = np.array(f['stimulus']['presentation'][ttl]['data'])
+    sweep = dataset.sweep(sweeps_to_analyze[i])
+    #filter the signal please
+    nyq = 250*1e3
+    cutoff = 2000
+    order = 3
+    fs = 500*1e3
+    def butter_lowpass_filter(data, cutoff, fs, order):
+        normal_cutoff = cutoff / nyq
+         # Get the filter coefficients 
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        y = filtfilt(b, a, data)
+        return y
+    #find peaks in the ttl signal 
+    peaks_ttl, nonesne = find_peaks(ttl, 1) 
+    v_filt = butter_lowpass_filter(sweep.v, cutoff, fs, order)
+    dv = np.gradient(v_filt, edge_order=1)
+    dt = np.gradient(sweep.t, edge_order=1)
+    dvdt = dv/dt
+    fig,ax = plt.subplots(3,1, sharex=True)
+    ax[0].plot(sweep.t*1e3, ttl)
+    ax[0].plot(sweep.t[peaks_ttl]*1e3, ttl[peaks_ttl],  'x')
+    ax[1].plot(sweep.t*1e3, v_filt)
+    ax[2].plot(sweep.t*1e3, dvdt)
+    t=sweep.t*1e3
+    #find differences between TTL peak and second peak in the derivative after peak
+    onsets = []
+    for j in range(0,len(peaks_ttl)):
+        left_border = peaks_ttl[j]+650
+        right_border =  peaks_ttl[j]+2000
+        aoi = dvdt[left_border:right_border]
+        threshold = int(input("Please provide derravitve threshold for event detection: "))
+        new_onset = []
+        while threshold > max(aoi) or new_onset == 'f' :
+            print('dvdt does not exceed threshold, please change thershold')
+            threshold = int(input("Please provide derravitve threshold for event detection: "))
+            onset_index = np.where(aoi>threshold)[0][0] 
+            ax[1].plot(t[left_border:right_border][onset_index], v_filt[left_border:right_border][onset_index], 'x')
+            print('printing new onset, press a button to continue')
+            while True:
+                if plt.waitforbuttonpress():
+                    break 
+            new_onset = input('New onset good? (p/f)')
+        print('analyzing event, press a button to continue')
+        onset = 'f'
+        while onset == 'f':
+            onset_index = np.where(aoi>threshold)[0][0] 
+            ax[1].plot(t[left_border:right_border][onset_index], v_filt[left_border:right_border][onset_index], 'x')
+            while True:
+                if plt.waitforbuttonpress():
+                    break 
+            onset = input('Onset good? (p/f)')
+            if onset == 'f':
+                threshold = int(input("Please provide derravitve threshold for event detection: "))
+        onsets.append(t[left_border:right_border][onset_index]-t[peaks_ttl[j]])
+        sweeps.append(j)
+    onsets_df = pd.DataFrame(onsets, sweeps)
+onsets_df['condition'] = condition
+onsets_df['file_name'] = f.filename
+onsets_df['species']   = species
+
+save_name = f.filename[82:101]+'.csv'
+onsets_df.to_csv(r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\\' + save_name)
+
+#%%AVERAGE OVER SWEEPS
+
+print('select file from the list')
+file = filedialog.askopenfilename()
+condition = []
+while condition != 'ZD' and condition != 'control':
+    condition = input('which protocol are you analyzing?, ZD or control?')
+    if condition == 'ZD':
+        cond = 'TTL_test_ZD'
+    elif condition == 'control':
+        cond = 'TTL_test_GAP'
+    else:
+        print('wrong input, try again')
+species = input('please provide species (human/mouse)')
+
+print('loading dataset')
+dataset= create_ephys_data_set(file)
+all_sweeps = dataset.filtered_sweep_table()
+print('fetching TTL data')
+#where is TTL??
+f = h5py.File(file, 'r')
+stim = pd.DataFrame(list(f['stimulus']['presentation'].keys()), columns=['ttl'])
+#create list with only TTL file names
+data_TTL = stim[stim['ttl'].str.contains('TTL')]
+
+#select subset of sweeps 
+subset = list(all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number)
+#convert to string for search
+strings = [str(x) for x in subset]
+#only select TTLs for subselected sweep numbers 
+selected_TTLs = data_TTL[data_TTL['ttl'].str.contains('|'.join(strings))].reset_index(drop=True)
+#select sweeps to analyze
+sweeps_to_analyze = all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number.reset_index(drop=True)
+  
+fig,ax = plt.subplots(3,1, sharex=True)     
+average_list = []
+average_list_dvdt = []
+for i in range(1,len(sweeps_to_analyze)):
+    sweeps = []
+    ttl = selected_TTLs.ttl[i]
+    ttl = np.array(f['stimulus']['presentation'][ttl]['data'])
+    sweep = dataset.sweep(sweeps_to_analyze[i])
+    #filter the signal please
+    nyq = 250*1e3
+    cutoff = 5000
+    order = 2
+    fs = 500*1e3
+    def butter_lowpass_filter(data, cutoff, fs, order):
+        normal_cutoff = cutoff / nyq
+         # Get the filter coefficients 
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        y = filtfilt(b, a, data)
+        return y
+    #find peaks in the ttl signal 
+    peaks_ttl, nonesne = find_peaks(ttl, 1) 
+    v_filt = butter_lowpass_filter(sweep.v, cutoff, fs, order)
+    dv = np.gradient(v_filt, edge_order=1)
+    dt = np.gradient(sweep.t, edge_order=1)
+    dvdt = dv/dt
+    ax[0].plot(sweep.t*1e3, ttl, color='k')
+    ax[0].plot(sweep.t[peaks_ttl]*1e3, ttl[peaks_ttl],  'x', color='red')
+    ax[1].plot(sweep.t*1e3, v_filt, color='lightgrey')
+    ax[2].plot(sweep.t*1e3, dvdt, color='k')
+    #calculate and make an average
+    average_list.append(v_filt)
+    average_list_dvdt.append(dvdt)
+average = np.mean(average_list, axis=0)
+average_dvdt = np.mean(average_list_dvdt, axis=0)
+ax[1].plot(sweep.t*1e3, average, color='red')
+ax[2].plot(sweep.t*1e3, average_dvdt, color='red')
+
+#%%detect average events per set 
+fig,ax = plt.subplots(3,1, sharex=True)     
+ax[0].plot(sweep.t*1e3, ttl, color='k')
+ax[1].plot(sweep.t*1e3, average, color='k')
+ax[2].plot(sweep.t*1e3, average_dvdt, color='k')
+ax[2].set_ylim(-1000, 1000)
+peaks_ttl, nonesne = find_peaks(ttl, 1) 
+t=sweep.t*1e3
+pulse_number = []
+onsets = []
+rise   = []
+qc_list = []
+for j in range(0,len(peaks_ttl)):
+    left_border = peaks_ttl[j]+1000
+    right_border =  peaks_ttl[j]+5000
+    right_border2 = right_border+20000
+    aoi = average_dvdt[left_border:right_border]
+    aoi2_v = average[left_border:(right_border+20000)]
+    aoi2_t = t[left_border:right_border2]
+    threshold = int(input("Please provide derravitve threshold for event detection: "))
+    new_onset = 'f'
+    if threshold > max(aoi):
+        print('dvdt does not exceed threshold, please change thershold')
+        print('max aoi is do not exceed this value again', max(aoi))
+        while new_onset == 'f':
+            threshold = int(input("Please provide derravitve threshold for event detection: "))
+            onset_index = np.where(aoi>threshold)[0][0] 
+            ax[1].plot(t[left_border:right_border][onset_index], average[left_border:right_border][onset_index], 'x')
+            peak_temp, nonsense = find_peaks(aoi2_v, prominence=.3)
+            rise.append(aoi2_t[peak_temp]-t[left_border:right_border][onset_index])
+            ax[1].plot(t[left_border:right_border2][peak_temp], average[left_border:right_border2][peak_temp], 'x')
+            print('printing new onset, press a button to continue')
+            while True:
+                if plt.waitforbuttonpress():
+                    break 
+            new_onset = input('New onset good? (p/f)')
+            qc = input('Does the event qc pass? ')
+            qc_list.append(qc)
+            onsets.append(t[left_border:right_border][onset_index]-t[peaks_ttl[j]])
+            pulse_number.append(j)
+            print('analyzing event, press a button to continue')
+    elif threshold < max(aoi):
+        onset = 'f'
+        while onset == 'f':
+            onset_index = np.where(aoi>threshold)[0][0] 
+            ax[1].plot(t[left_border:right_border][onset_index], average[left_border:right_border][onset_index], 'x')
+            while True:
+                if plt.waitforbuttonpress():
+                    break 
+            onset = input('Onset good? (p/f)')
+            if onset == 'f':
+                threshold = int(input("Please provide derravitve threshold for event detection: "))
+        qc = input('Does the event qc pass? ')
+        onsets.append(t[left_border:right_border][onset_index]-t[peaks_ttl[j]])
+        pulse_number.append(j)
+        qc_list.append(qc)
+onsets_df = pd.DataFrame(onsets)
+onsets_df['condition'] = condition
+onsets_df['file_name'] = f.filename
+onsets_df['species']   = species
+onsets_df['n_pulse']   = pulse_number
+onsets_df['qc']        = qc_list
+
+if species == 'human':
+    save_name = f.filename[82:101]+'.csv'
+    onsets_df.to_csv(r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\\' + save_name)
+elif species == 'mouse':
+    save_name = f.filename[77:102]+'.csv'
+    onsets_df.to_csv(r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\\' + save_name)
+
+    

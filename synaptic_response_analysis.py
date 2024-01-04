cead6ba7df92caee35b2e014bdd6718b92f7b2d7
StanDriessens <standriessens@gmail.com>
2024-01-04 16:58:33 +0100

    New file based on concept script but with improvements and a gui interface to select the correct protocols

--- synaptic_response_analysis.py
+++ synaptic_response_analysis.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Sep  7 08:57:04 2023
+Created on Thu Jan  4 16:40:44 2024
 
 @author: sdr267
 """
+
 
 from pathlib import Path
 import seaborn as sns
@@ -35,139 +36,6 @@ from tkinter import filedialog
 from tkinter import messagebox as mb
 from tkinter import Radiobutton
 
-#%% SINGLE EVENT DETECION
-print('select file from the list')
-file = filedialog.askopenfilename()
-condition = []
-while condition != 'ZD' and condition != 'control':
-    condition = input('which protocol are you analyzing?, ZD or control?')
-    if condition == 'ZD':
-        cond = 'TTL_test_ZD'
-    elif condition == 'control':
-        cond = 'TTL_test_gap'
-    else:
-        print('wrong input, try again')
-species = input('please provide species (human/mouse)')
-
-print('loading dataset')
-dataset= create_ephys_data_set(file)
-all_sweeps = dataset.filtered_sweep_table()
-print('fetching TTL data')
-#where is TTL??
-f = h5py.File(file, 'r')
-stim = pd.DataFrame(list(f['stimulus']['presentation'].keys()), columns=['ttl'])
-#create list with only TTL file names
-data_TTL = stim[stim['ttl'].str.contains('TTL')]
-#select subset of sweeps 
-subset = list(all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number)
-#convert to string for search
-strings = [str(x) for x in subset]
-#only select TTLs for subselected sweep numbers 
-selected_TTLs = data_TTL[data_TTL['ttl'].str.contains('|'.join(strings))].reset_index(drop=True)
-"""
-work in progress to make sure TTL matches the correct sweep incase wrong gap free was used 
-
-works now 
-"""
-
-
-if condition == 'control':
-    ttl_accept = []
-    sweeps_to_analyze = []
-    for i in selected_TTLs.ttl:
-        temp = str(f['stimulus']['presentation'][i].attrs['stimulus_description'])
-        if '10hz' in temp:
-            sweep_number = f['stimulus']['presentation'][i].attrs['sweep_number']
-            DA_sweep = all_sweeps.iloc[sweep_number]
-            if 'TTL' in DA_sweep.stimulus_code and 'ZD' not in DA_sweep.stimulus_code and  'inbetwe' not in DA_sweep.stimulus_code:
-                ttl_accept.append(i)
-                sweeps_to_analyze.append(int(DA_sweep.sweep_number))
-                
-if condition == 'ZD':
-    ttl_accept = []
-    sweeps_to_analyze = []
-    for i in selected_TTLs.ttl:
-        temp = str(f['stimulus']['presentation'][i].attrs['stimulus_description'])
-        if '10hz' in temp:
-            sweep_number = f['stimulus']['presentation'][i].attrs['sweep_number']
-            DA_sweep = all_sweeps.iloc[sweep_number]
-            if 'ZD' in DA_sweep.stimulus_code  and  'inbetwe' not in DA_sweep.stimulus_code:
-                ttl_accept.append(i)
-                sweeps_to_analyze.append(int(DA_sweep.sweep_number))           
-
-
-
-
-#select sweeps to analyze
-#sweeps_to_analyze = all_sweeps.loc[all_sweeps['stimulus_code']==cond].sweep_number.reset_index(drop=True)
-
-response_onset = pd.DataFrame()
-for i in range(0,len(sweeps_to_analyze)):
-    sweeps = []
-    ttl = selected_TTLs.ttl[i]
-    ttl = np.array(f['stimulus']['presentation'][ttl]['data'])
-    sweep = dataset.sweep(sweeps_to_analyze[i])
-    #filter the signal please
-    nyq = 250*1e3
-    cutoff = 2000
-    order = 3
-    fs = 500*1e3
-    def butter_lowpass_filter(data, cutoff, fs, order):
-        normal_cutoff = cutoff / nyq
-         # Get the filter coefficients 
-        b, a = butter(order, normal_cutoff, btype='low', analog=False)
-        y = filtfilt(b, a, data)
-        return y
-    #find peaks in the ttl signal 
-    peaks_ttl, nonesne = find_peaks(ttl, 1) 
-    v_filt = butter_lowpass_filter(sweep.v, cutoff, fs, order)
-    dv = np.gradient(v_filt, edge_order=1)
-    dt = np.gradient(sweep.t, edge_order=1)
-    dvdt = dv/dt
-    fig,ax = plt.subplots(3,1, sharex=True)
-    ax[0].plot(sweep.t*1e3, ttl)
-    ax[0].plot(sweep.t[peaks_ttl]*1e3, ttl[peaks_ttl],  'x')
-    ax[1].plot(sweep.t*1e3, v_filt)
-    ax[2].plot(sweep.t*1e3, dvdt)
-    t=sweep.t*1e3
-    #find differences between TTL peak and second peak in the derivative after peak
-    onsets = []
-    for j in range(0,len(peaks_ttl)):
-        left_border = peaks_ttl[j]+650
-        right_border =  peaks_ttl[j]+2000
-        aoi = dvdt[left_border:right_border]
-        threshold = int(input("Please provide derravitve threshold for event detection: "))
-        new_onset = []
-        while threshold > max(aoi) or new_onset == 'f' :
-            print('dvdt does not exceed threshold, please change thershold')
-            threshold = int(input("Please provide derravitve threshold for event detection: "))
-            onset_index = np.where(aoi>threshold)[0][0] 
-            ax[1].plot(t[left_border:right_border][onset_index], v_filt[left_border:right_border][onset_index], 'x')
-            print('printing new onset, press a button to continue')
-            while True:
-                if plt.waitforbuttonpress():
-                    break 
-            new_onset = input('New onset good? (p/f)')
-        print('analyzing event, press a button to continue')
-        onset = 'f'
-        while onset == 'f':
-            onset_index = np.where(aoi>threshold)[0][0] 
-            ax[1].plot(t[left_border:right_border][onset_index], v_filt[left_border:right_border][onset_index], 'x')
-            while True:
-                if plt.waitforbuttonpress():
-                    break 
-            onset = input('Onset good? (p/f)')
-            if onset == 'f':
-                threshold = int(input("Please provide derravitve threshold for event detection: "))
-        onsets.append(t[left_border:right_border][onset_index]-t[peaks_ttl[j]])
-        sweeps.append(j)
-    onsets_df = pd.DataFrame(onsets, sweeps)
-onsets_df['condition'] = condition
-onsets_df['file_name'] = f.filename
-onsets_df['species']   = species
-
-save_name = f.filename[82:101]+'.csv'
-onsets_df.to_csv(r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\\' + save_name)
 
 #%%AVERAGE OVER SWEEPS
 #initialize analysis 
@@ -179,40 +47,36 @@ species_select = ['human', 'mouse']
 def save_species():
     global species
     species = species_select[x.get()]
-    window.destroy()
-window = Tk()
+    window1.destroy()
+window1 = Tk()
 x = IntVar()
 for index in range(len(species_select)):
-    radiobutton = Radiobutton(window, text=species_select[index], variable=x, value=index, padx = 25)
+    radiobutton = Radiobutton(window1, text=species_select[index], variable=x, value=index, padx = 25)
     radiobutton.pack(anchor=W)
-okay_button = Button(window, text="Select Species", command=save_species())
+okay_button = Button(window1, text="Select Species", command=save_species)
 okay_button.pack(pady=10)    
-window.mainloop()    
+window1.mainloop()    
 
-
-
+#load in the dataset 
 print('loading dataset')
 dataset= create_ephys_data_set(file)
 all_sweeps = dataset.filtered_sweep_table()
 
 #get the correct sweep name from tkinter button
 sets_unique = list(np.unique(all_sweeps.stimulus_code))
-
 def save_selected_value():
     global cond
     cond = sets_unique[x.get()]
-    window.destroy()
-    
-
-window = Tk()
+    window2.destroy()
+window2 = Tk()
 x = IntVar()
 for index in range(len(sets_unique)):
-    radiobutton = Radiobutton(window, text=sets_unique[index], variable=x, value=index, padx = 25)
+    radiobutton = Radiobutton(window2, text=sets_unique[index], variable=x, value=index, padx = 25)
     radiobutton.pack(anchor=W)
-okay_button = Button(window, text="Select sweep protocol", command=save_selected_value)
+okay_button = Button(window2, text="Select sweep protocol", command=save_selected_value)
 okay_button.pack(pady=10)    
 
-window.mainloop()
+window2.mainloop()
     
 
 print('fetching TTL data')
@@ -230,7 +94,7 @@ strings = [str(x) for x in subset]
 selected_TTLs = data_TTL[data_TTL['ttl'].str.contains('|'.join(strings))].reset_index(drop=True)
 #select sweeps to analyze
 
-if condition == 'control':
+if 'control' in cond:
     ttl_accept = []
     sweeps_to_analyze = []
     for i in selected_TTLs.ttl:
@@ -242,7 +106,7 @@ if condition == 'control':
                 ttl_accept.append(i)
                 sweeps_to_analyze.append(int(DA_sweep.sweep_number))
                 
-if condition == 'ZD':
+if 'ZD' in cond:
     ttl_accept = []
     sweeps_to_analyze = []
     for i in selected_TTLs.ttl:
@@ -252,7 +116,10 @@ if condition == 'ZD':
             DA_sweep = all_sweeps.iloc[sweep_number]
             if 'ZD' in DA_sweep.stimulus_code  and  'inbetwe' not in DA_sweep.stimulus_code:
                 ttl_accept.append(i)
-                sweeps_to_analyze.append(int(DA_sweep.sweep_number))              
+                sweeps_to_analyze.append(int(DA_sweep.sweep_number))    
+else:
+    print('analysis has stopped no correct protocol selected')
+    
 
 fig,ax = plt.subplots(3,1, sharex=True)     
 average_list = []
@@ -428,7 +295,3 @@ if species == 'human':
 elif species == 'mouse':
     save_name = f.filename[77:102]+'.csv'
     onsets_df.to_csv(r'C:\Users\sdr267\Documents\PhD\ProjectSynapticConnections\Ih_experiment\events\\' + save_name)
-
-#%%
-
-from  get_PSP_param import get_synaptic_params
